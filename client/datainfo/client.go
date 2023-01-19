b46a161ad397de54ad9d3af0d3f2607fb92124ab
Ian McEwen <mian@cyverse.org>
2023-01-19 01:15:28 -0700

    Fill out data-info client thoroughly

--- client/datainfo/client.go
+++ client/datainfo/client.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 
@@ -41,6 +42,33 @@ func (d *DataInfoClient) uriPath(ctx context.Context, pathParts ...string) (stri
 	return uri.String(), nil
 }
 
+func (d *DataInfoClient) reqJSON(ctx context.Context, method, uri string, body io.Reader, target any) error {
+	req, err := http.NewRequestWithContext(ctx, method, uri, body)
+	if err != nil {
+		return errors.Wrap(err, "Failed creating request with context")
+	}
+	if body != nil {
+		req.Header.Set("content-type", "application/json")
+	}
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return errors.Wrap(err, "Failed requesting URL")
+	} else if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		return restutils.NewHTTPError(resp.StatusCode, fmt.Sprintf("GET %s returned %d", uri, resp.StatusCode))
+	}
+	defer resp.Body.Close()
+
+	if target != nil {
+		err = json.NewDecoder(resp.Body).Decode(target)
+		if err != nil {
+			return errors.Wrap(err, "Failed decoding JSON")
+		}
+	}
+	return err
+
+}
+
 // Create IRODS Group
 func (d *DataInfoClient) CreateGroup(ctx context.Context, name string, members []string) (Group, error) {
 	var g Group
@@ -56,40 +84,48 @@ func (d *DataInfoClient) CreateGroup(ctx context.Context, name string, members [
 		return g, errors.Wrap(err, "Failed to marshal group to create")
 	}
 
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, uri, bytes.NewBuffer(msg))
-	if err != nil {
-		return g, errors.Wrap(err, "Failed to create request to create group")
-	}
-	req.Header.Set("content-type", "application/json")
-
-	resp, err := httpClient.Do(req)
-	if err != nil {
-		return g, errors.Wrap(err, "Failed in request to data-info")
-	} else if resp.StatusCode < 200 || resp.StatusCode > 299 {
-		return g, restutils.NewHTTPError(resp.StatusCode, fmt.Sprintf("%s returned %d", uri, resp.StatusCode))
-	}
-	defer resp.Body.Close()
-
-	err = json.NewDecoder(resp.Body).Decode(&g)
+	err = d.reqJSON(ctx, http.MethodPost, uri, bytes.NewBuffer(msg), &g)
 	return g, err
 }
 
 // List Group Members
 func (d *DataInfoClient) ListGroupMembers(ctx context.Context, name string) (Group, error) {
 	var g Group
-	// GET /:group-name/
-	return g, nil
+
+	uri, err := d.uriPath(ctx, "groups", name)
+	if err != nil {
+		return g, errors.Wrap(err, "Failed to build URL")
+	}
+
+	err = d.reqJSON(ctx, http.MethodGet, uri, nil, &g)
+	return g, err
 }
 
 // Update Group Members
-func (d *DataInfoClient) UpdateGroupMembers(ctx context.Context, name string) (Group, error) {
+func (d *DataInfoClient) UpdateGroupMembers(ctx context.Context, name string, members []string) (Group, error) {
 	var g Group
-	// PUT /:group-name/
-	return g, nil
+
+	uri, err := d.uriPath(ctx, "groups", name)
+	if err != nil {
+		return g, errors.Wrap(err, "Failed to build URL")
+	}
+
+	postGroup := Group{Name: name, Members: members}
+	msg, err := json.Marshal(postGroup)
+	if err != nil {
+		return g, errors.Wrap(err, "Failed to marshal group to create")
+	}
+
+	err = d.reqJSON(ctx, http.MethodPut, uri, bytes.NewBuffer(msg), &g)
+	return g, err
 }
 
 // Delete Group
 func (d *DataInfoClient) DeleteGroup(ctx context.Context, name string) error {
-	// DELETE /:group-name/
-	return nil
+	uri, err := d.uriPath(ctx, "groups", name)
+	if err != nil {
+		return errors.Wrap(err, "Failed to build URL")
+	}
+
+	return d.reqJSON(ctx, http.MethodDelete, uri, nil, nil)
 }
