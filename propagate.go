6a9558df607c121760e9f24b8ce21eb67f88f1dd
Ian McEwen <mian@cyverse.org>
2023-01-19 03:49:06 -0700

    Flesh out PropagateGroupById

--- propagate.go
+++ propagate.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/pkg/errors"
+
+	"github.com/cyverse-de/go-mod/restutils"
 	"github.com/cyverse-de/group-propagator/client/datainfo"
 	"github.com/cyverse-de/group-propagator/client/groups"
 )
@@ -30,11 +36,47 @@ func NewPropagator(groupsClient groups.GroupsClient, groupPrefix string, dataInf
 	}
 }
 
-/*
-func (p *Propagator) PropagateGroupById(groupId string) error {
-	// get group by ID
-	// get group members
-	// create group if necessary
-	// update group memberships
+func (p *Propagator) PropagateGroupById(ctx context.Context, groupID string) error {
+	g, err := p.groupsClient.GetGroupByID(ctx, groupID)
+	if err != nil {
+		return errors.Wrap(err, "Failed fetching Grouper group by ID")
+	}
+
+	members, err := p.groupsClient.GetGroupMembers(ctx, g.Name)
+	if err != nil {
+		return errors.Wrap(err, "Failed fetching Grouper group members")
+	}
+
+	var irodsMembers []string
+	for _, member := range members.Members {
+		irodsMembers = append(irodsMembers, member.ID)
+	}
+
+	irodsName := fmt.Sprintf("%s%s", p.groupPrefix, g.ID)
+
+	irodsGroupExists := true
+
+	// Check if group exists/has members, but we don't need to care what members
+	_, err = p.dataInfoClient.ListGroupMembers(ctx, irodsName)
+	if restutils.GetStatusCode(err) == 404 {
+		irodsGroupExists = false
+	} else if err != nil {
+		return errors.Wrap(err, "Failed fetching existing iRODS group members")
+	}
+
+	var finalGroup datainfo.Group
+
+	if irodsGroupExists {
+		finalGroup, err = p.dataInfoClient.UpdateGroupMembers(ctx, irodsName, irodsMembers)
+	} else {
+		finalGroup, err = p.dataInfoClient.CreateGroup(ctx, irodsName, irodsMembers)
+	}
+
+	if err != nil {
+		return errors.Wrapf(err, "Failed creating or updating group with %d members", len(irodsMembers))
+	}
+
+	log.Infof("Updated group %s (%s) -> %s with %d members", g.Name, groupID, finalGroup.Name, len(finalGroup.Members))
+
+	return nil
 }
-*/
